04df1c519b762a7eb883b52609acfcdd9affc5d8
Gopal <60707591+gopi-AI@users.noreply.github.com>
2025-07-03 01:56:12 +0530

    Update Member.js

--- backend/models/Member.js
+++ backend/models/Member.js
@@ -1,1 +1,14 @@
+const mongoose = require('mongoose');
+
+const memberSchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+  weight: Number,
+  age: Number,
+  sex: String,
+  sports: [String],
+  team: String
+});
+
+module.exports = mongoose.model('Member', memberSchema);
 
