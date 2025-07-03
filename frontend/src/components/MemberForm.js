95240ed2005934b9154b7b43f24a6c0424734717
Gopal <60707591+gopi-AI@users.noreply.github.com>
2025-07-03 21:08:22 +0530

    Update MemberForm.js

--- frontend/src/components/MemberForm.js
+++ frontend/src/components/MemberForm.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+ 
 function MemberForm() {
   const navigate = useNavigate();
 
