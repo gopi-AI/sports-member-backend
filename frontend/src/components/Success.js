6bcd6251a97f71b34ff1fbc5064530f45f59ce71
Gopal <60707591+gopi-AI@users.noreply.github.com>
2025-07-03 20:55:24 +0530

    Create Success.js

---
+++ frontend/src/components/Success.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Success() {
+  return (
+    <div className="container py-5 text-center">
+      <div className="alert alert-success">
+        <h2 className="mb-4">✅ Member Registered Successfully!</h2>
+        <Link className="btn btn-primary" to="/">
+          Register Another
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Success;
