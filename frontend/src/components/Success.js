c5d9dbc5f395110cd111984ebac290eaa92a2aef
Gopal <60707591+gopi-AI@users.noreply.github.com>
2025-07-03 21:33:43 +0530

    Update Success.js

--- frontend/src/components/Success.js
+++ frontend/src/components/Success.js
@@ -6,9 +6,7 @@ function Success() {
     <div className="container py-5 text-center">
       <div className="alert alert-success">
         <h2 className="mb-4">✅ Member Registered Successfully!</h2>
-        <Link className="btn btn-primary" to="/">
-          Register Another
-        </Link>
+       
       </div>
     </div>
   );
