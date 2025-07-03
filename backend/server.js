9879de87fd4dcbcb2079aa1f54d3c4bfbd1546b7
Gopal <60707591+gopi-AI@users.noreply.github.com>
2025-07-03 20:39:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: 'https://sports-member-backend-61jc.vercel.app',
+  origin: 'https://oxygenfitness.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
