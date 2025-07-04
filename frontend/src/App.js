3d3f73898ce10eb27a152d0ce8d45b8c46d2c996
Gopal <60707591+gopi-AI@users.noreply.github.com>
2025-07-04 12:03:03 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,30 +5,23 @@ import Dashboard from './admin/Dashboard';
 import TeamProfile from './admin/TeamProfile';
 import Success from "./components/Success";
 
-   
-  
 function App() {
   return (
     <div className="container py-4">
-      <nav className="mb-4">
-       <img 
-    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc_WxcS1J_6tayj3HFh9apGgn9j-U71LA09w&s"
-    class="rounded-circle img-thumbnail"
-    style="width:120px; height:120px; object-fit:cover;"
-    alt="Profile"
-  />
-        
+      <nav className="mb-4 text-center">
+        <img 
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc_WxcS1J_6tayj3HFh9apGgn9j-U71LA09w&s"
+          className="rounded-circle img-thumbnail"
+          style={{ width: "120px", height: "120px", objectFit: "cover" }}
+          alt="Profile"
+        />
       </nav>
       <Routes>
-        <Route path="/team/:teamName" element={<TeamProfile />} />
         <Route path="/" element={<MemberForm />} />
+        <Route path="/team/:teamName" element={<TeamProfile />} />
         <Route path="/success" element={<Success />} />
         <Route path="/admin" element={<Dashboard />} />
       </Routes>
-      
-       
-  
-
     </div>
   );
 }
