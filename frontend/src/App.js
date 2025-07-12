1b60838734af7a5a9cc834b97d6495950d6ddd39
Gopal <60707591+gopi-AI@users.noreply.github.com>
2025-07-12 18:31:43 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
         <img 
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc_WxcS1J_6tayj3HFh9apGgn9j-U71LA09w&s"
           className="rounded-circle img-thumbnail"
-          style={{ width: "140px", height: "140px", objectFit: "cover" }}
+          style={{ width: "100px", height: "100px", objectFit: "cover" }}
           alt="Profilepic"
         />
       </nav>
